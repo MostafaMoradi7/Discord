6ce9330ba4fdd3c8b373b9c84cf1bcf938bc3764
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2022-05-31 10:34:35 +0430

    main body of messages in channels designed and almost completed.

--- src/MessageOperations/ChannelMessage.java
+++ src/MessageOperations/ChannelMessage.java
@@ -1,6 +1,51 @@
 package MessageOperations;
 
+import ClientOperations.Client;
+import Services.Type;
+
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class ChannelMessage implements Serializable {
+    private String channelName;
+    private String pinnedMessage;
+    private Client mainADMIN;
+    private Type type;
+    private ArrayList<Client> members;
+    private ArrayList<Client> admins;
+    private ArrayList<ChannelMessage> messages;
+
+    public ChannelMessage(String channelName, String pinnedMessage, Client mainADMIN, Type type) {
+        this.channelName = channelName;
+        this.pinnedMessage = pinnedMessage;
+        this.mainADMIN = mainADMIN;
+        this.type = type;
+        members = new ArrayList<>();
+        admins = new ArrayList<>();
+        messages = new ArrayList<>();
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public String getPinnedMessage() {
+        return pinnedMessage;
+    }
+
+    public Client getMainADMIN() {
+        return mainADMIN;
+    }
+
+    public ArrayList<Client> getMembers() {
+        return members;
+    }
+
+    public ArrayList<Client> getAdmins() {
+        return admins;
+    }
+
+    public ArrayList<ChannelMessage> getMessages() {
+        return messages;
+    }
 }
