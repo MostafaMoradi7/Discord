cd7f6bd45354ab33cfe55ea551793e3f4d1682b8
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2022-05-31 10:33:06 +0430

    main body designed

--- src/MessageOperations/GroupMessage.java
+++ src/MessageOperations/GroupMessage.java
@@ -1,6 +1,12 @@
 package MessageOperations;
 
+import ClientOperations.Client;
+
 import java.io.Serializable;
 
 public class GroupMessage implements Serializable {
+    private String name;
+    private Client from;
+    private Object body;
+
 }
