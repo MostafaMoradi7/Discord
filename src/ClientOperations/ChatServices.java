68cd255b73265d56f2aaa67855c57e89f3129d72
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2022-06-25 22:41:33 +0430

    whenever a service was needed to be provided it'll get out of null value so that through run method the communication will happen.

---
+++ src/ClientOperations/ChatServices.java
@@ -0,0 +1,38 @@
+package ClientOperations;
+
+import Services.Channel;
+import Services.Group;
+import Services.PrivateChat;
+
+public class ChatServices implements Runnable{
+    private ClientHandler clientHandler;
+    private Client client;
+    private PrivateChat pvChat;
+    private Group gapChat;
+    private Channel channelChat;
+
+
+    public ChatServices(ClientHandler clientHandler, Client client){
+        this.clientHandler = clientHandler;
+        this.client = client;
+    }
+
+    public void setPvChat(PrivateChat pvChat){
+        this.pvChat = pvChat;
+    }
+
+    public void setGapChat(Group gapChat){
+        this.gapChat = gapChat;
+    }
+
+    public void setChannelChat(Channel channelChat){
+        this.channelChat = channelChat;
+    }
+
+
+
+    @Override
+    public void run() {
+
+    }
+}
