49721c501762d0f180923bcbe3e4b0beb4b4c3ca
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2022-06-27 23:23:24 +0430

    out put handler and input handler organized for receiving and sending data

---
+++ src/Chat/ChatInputHandler.java
@@ -0,0 +1,45 @@
+package Chat;
+
+import ClientOperations.PortableData;
+import MessageOperations.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+public class ChatInputHandler implements Runnable{
+    private Socket clientSocket;
+    private Chat chat;
+    private ObjectInputStream reader;
+    private static boolean isRunning = true;
+
+    public ChatInputHandler(Chat chat, Socket clientSocket) {
+        this.clientSocket = clientSocket;
+        this.chat = chat;
+        try {
+            reader = new ObjectInputStream(clientSocket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setIsRunning(boolean isRunning) {
+        ChatInputHandler.isRunning = isRunning;
+    }
+
+    @Override
+    public void run() {
+        while (isRunning) {
+            try {
+                PortableData receivedData = (PortableData) reader.readObject();
+                if (!receivedData.getOrder().equals("unsuccessful")){
+                    chat.receiveMessage(receivedData.getObject());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
