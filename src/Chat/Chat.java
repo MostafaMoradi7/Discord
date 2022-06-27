f54d732b1650ff648d72d7a9a97463cbaf7cab95
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2022-06-27 23:20:46 +0430

    some inheritance added

---
+++ src/Chat/Chat.java
@@ -0,0 +1,20 @@
+package Chat;
+
+import MessageOperations.Message;
+
+import java.net.Socket;
+
+public class Chat implements Runnable{
+
+    protected ChatInputHandler chatInputHandler;
+    protected ChatOutputHandler chatOutputHandler;
+    protected Message message;
+    @Override
+    public void run() {
+
+    }
+
+    protected void receiveMessage(Object message){
+        this.message = (Message) message;
+    }
+}
